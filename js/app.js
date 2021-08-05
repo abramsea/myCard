80c6c55c1c40642462624544136f1eb551bb1df1
Evgenii Abramson <abramson.ea@gmail.com>
2021-08-05 18:29:38 +0300

    Update 2021-08-05T15:29:31.247Z

--- js/app.js
+++ js/app.js
@@ -1,47 +1,48 @@
-// const cardsContainer = document.querySelector( 'div.cards' );
+const json = `{
+	"pk1": {
+		"title": "pk1",
+		"link": "https://pk1.spb.ru",
+		"github": null,
+		"image": "images/pk1.png",
+		"lead": "lorem ipsum test testingson",
+		"tech": "HTML, CSS, JS"
+	},
+	"pk2": {
+		"title": "pk2",
+		"link": "https://pk1.spb.ru",
+		"github": null,
+		"image": "images/pk1.png",
+		"lead": "lorem ipsum test testingson",
+		"tech": "HTML, CSS, JS"
+	}
+}`
 
-// const readTextFile = ( file, callback ) => {
-//     const rawFile = new XMLHttpRequest();
+const info = JSON.parse( json );
 
-//     console.log( rawFile );
+const card = (
+    <li className="card">
+		<a className="card__pic-box" href={info[0].link} target="_blank">
+            <img className="card__pic" src={info[0].image} alt={title}></img>
+        </a>
+        <div className="card__darken">
+            <h3 className="card__title">{info[0].title}</h3>
+            <p className="card__tech">
+                {info[0].lead}
+            </p>
+            <p className="card__tech">
+                {info[0].tech}
+            </p>
+        </div>
+        <div className="card__info">
+            <a href={info[0].github} className="icon" target="_blank">
+                <i className="fab fa-github"></i>
+            </a>
+            <a href={info[0].link} className="icon" target="_blank">
+                <i className="fas fa-external-link-alt"></i>
+            </a>
+        </div>
+    </li>);
 
-//     rawFile.overrideMimeType( 'application/json' );
-//     rawFile.open( 'GET', file, true );
-//     rawFile.onreadystatechange = () => {
-//         if ( rawFile.readyState === 4 && rawFile.status == '200' ) {
-//             callback( rawFile.responseText );
-//         }
-//         rawFile.send( null );
-//     }
+const cardList = document.querySelector( 'ul.cards' );
 
-//     console.log( rawFile );
-// }
-
-// const cardsInfo = readTextFile( '../data/cards.json', ( text ) => {
-//     return JSON.parse( text );
-// })
-
-// console.log( cardsInfo );
-
-
-const json = `{ 
-    "cards" : [{
-        "title" : "акустика Jamo",
-        "lead" : "Landing page",
-        "tech" : "HTML, CSS, JS",
-        "link" : "https://abramsea.github.io/jamo",
-        "image_src" : "img/jamo.jpg",
-        "github": "https://github.com/abramsea/jamo"
-    },
-    {
-        "title" : "акустика Jamo",
-        "lead" : "Landing page",
-        "tech" : "HTML, CSS, JS",
-        "link" : "https://abramsea.github.io/jamo",
-        "image_src" : "img/jamo.jpg",
-        "github": "https://github.com/abramsea/jamo"
-    }
-]}`
-const cards = JSON.parse( json );
-console.log(cards);
-
+ReactDOM.render( card, cardList );
