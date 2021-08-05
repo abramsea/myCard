ae422293c2c7d073d591a7133d1ca2db842aced2
Evgenii Abramson <abramson.ea@gmail.com>
2021-08-05 18:31:32 +0300

    Update 2021-08-05T15:31:30.550Z

--- js/app.js
+++ js/app.js
@@ -21,23 +21,23 @@ const info = JSON.parse( json );
 
 const card = (
     <li className="card">
-		<a className="card__pic-box" href={info[0].link} target="_blank">
-            <img className="card__pic" src={info[0].image} alt={title}></img>
+		<a className="card__pic-box" href={info[pk1].link} target="_blank">
+            <img className="card__pic" src={info[pk1].image} alt={title}></img>
         </a>
         <div className="card__darken">
-            <h3 className="card__title">{info[0].title}</h3>
+            <h3 className="card__title">{info[pk1].title}</h3>
             <p className="card__tech">
-                {info[0].lead}
+                {info[pk1].lead}
             </p>
             <p className="card__tech">
-                {info[0].tech}
+                {info[pk1].tech}
             </p>
         </div>
         <div className="card__info">
-            <a href={info[0].github} className="icon" target="_blank">
+            <a href={info[pk1].github} className="icon" target="_blank">
                 <i className="fab fa-github"></i>
             </a>
-            <a href={info[0].link} className="icon" target="_blank">
+            <a href={info[pk1].link} className="icon" target="_blank">
                 <i className="fas fa-external-link-alt"></i>
             </a>
         </div>
