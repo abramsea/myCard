31f768a0426e875029623aa95da8018ec22a2253
Evgenii Abramson <abramson.ea@gmail.com>
2021-08-06 18:58:59 +0300

    Update 2021-08-06T15:58:57.853Z

--- js/app.js
+++ js/app.js
@@ -43,7 +43,7 @@ const projectsJSON = `{
 		"title": "Mesto",
 		"link": "https://abramsea.github.io/spr11/index.htm",
 		"github": "https://github.com/abramsea/spr11",
-		"image": "images/analyzer.jpg",
+		"image": "images/mesto.jpg",
 		"lead":  "Проект Яндекс.Практикум.",
 		"tech": "HTML, CSS, JS, Webpack"
 	}
@@ -51,100 +51,147 @@ const projectsJSON = `{
 
 const projectsInfo = JSON.parse( projectsJSON );
 
-const cardList = Object.values( projectsInfo ).map( item => item );
+const cards = Object.values( projectsInfo ).map( item => item );
 
-console.log(cardList)
+function CardList( props ) {
+	const cards = props.cards;
+	const items = cards.map( function ( item, index ) {
 
-// (<li className="card" key={item}>
-// 		<a className="card__pic-box" href={link} target="_blank">
-//             <img className="card__pic" src={image} alt={title}></img>
-//         </a>
-//         <div className="card__darken">
-//             <h3 className="card__title">{title}</h3>
-//             <p className="card__tech">
-//                 {lead}
-//             </p>
-//             <p className="card__tech">
-//                 {tech}
-//             </p>
-//         </div>
-//         <div className="card__info">
-//             <a href={github} className="icon" target="_blank">
-//                 <i className="fab fa-github"></i>
-//             </a>
-//             <a href={link} className="icon" target="_blank">
-//                 <i className="fas fa-external-link-alt"></i>
-//             </a>
-//         </div>
-//     </li>);
+		if ( item.github != null ) 
+		{
+			return (
+				<li className="card" key={index++}>
+					<picture className="card__pic-box">
+						<img className="card__pic" src={item.image} alt={item.title}></img>
+					</picture>
+					<div className="card__darken">
+						<h3 className="card__title">{item.title}</h3>
+						<p className="card__tech">
+							{item.lead}
+						</p>
+						<p className="card__tech">
+							{item.tech}
+						</p>
+					</div>
+					<div className="card__info">
+						<a href={item.github} className="icon" target="_blank">
+							<i className="fab fa-github"></i>
+						</a>
+						<a href={item.link} className="icon" target="_blank">
+							<i className="fas fa-external-link-alt"></i>
+						</a>
+					</div>
+				</li>
+			)
+		}
+		else
+		{
+			return (
+				<li className="card" key={index++}>
+					<picture className="card__pic-box">
+						<img className="card__pic" src={item.image} alt={item.title}></img>
+					</picture>
+					<div className="card__darken">
+						<h3 className="card__title">{item.title}</h3>
+						<p className="card__tech">
+							{item.lead}
+						</p>
+						<p className="card__tech">
+							{item.tech}
+						</p>
+					</div>
+					<div className="card__info">
+						<a href={item.link} className="icon" target="_blank">
+							<i className="fas fa-external-link-alt"></i>
+						</a>
+					</div>
+				</li>
+			)
+		}
+	});
 
-const projects = document.querySelector( '#projects' );
+	return (
+		<ul className="cards">{items}</ul>
+	);
+}
 
-// ReactDOM.render( <ul className="cards">{cardList}</ul>, projects );
+const projects = document.querySelector( '#projects' ); 
+
+ReactDOM.render( <CardList cards={cards}/>, projects ); 
+
+
+
+
 const timelineJSON = `{
 	"1": {
 		"date": "2019.01",
 		"title": "Сам себе режиссёр",
-		"lead": "Самостоятельное обучение на курсах HTML Academy, Stepik",
-		"isLast": false
+		"lead": "Самостоятельное обучение на курсах HTML Academy, Stepik"
 	},
 	"2": {
 		"date": "2019.09-2020.10",
 		"title": "ООО «Пульт.ру»",
-		"lead": "HTML-верстальщик",
-		"isLast": false
+		"lead": "HTML-верстальщик"
 	},
 	"3": {
 		"date": "2020.01-2020.08",
 		"title": "Яндекс.Практикум",
-		"lead": "Обучение на курсе Фронтенд-разработчик",
-		"isLast": false
+		"lead": "Обучение на курсе Фронтенд-разработчик"
 	},
 	"4": {
 		"date": "2020.11-2021.07",
 		"title": "Студия М18",
-		"lead": "Frontend Developer",
-		"isLast": false
+		"lead": "Frontend Developer"
 	},
 	"5": {
 		"date": "2021.08-н.в.",
 		"title": "ООО «Сберлогистика»",
-		"lead": "React Developer",
-		"isLast": true
+		"lead": "React Developer"
 	}
 }`
 
 const timelineInfo = JSON.parse( timelineJSON );
 
+const timelineRows = Object.values( timelineInfo ).map( item => item );
 
-// const timelineRow = (
-// 	<li className="growup-row" key={item}>
-// 		<div className="growup-left-col">
-// 			<p>
-// 				{date}
-// 			</p>
-// 		</div>
-// 		<div className="growup-right-col">
-// 			<div className="growup-right-col-wrapper">
-// 				<div className="timeline-line"></div>
-// 				<div className="timeline-wrapper">
-// 					<div className="timeline-circle-wrapper">
-// 						<div className="timeline-circle"></div>
-// 					</div>
-// 				</div>
-// 				<div className="growup-text-wrapper">
-// 					<h3 className="growup-title">
-// 						{title}
-// 					</h3>
-// 					<p>
-// 						{lead}
-// 					</p>
-// 				</div>
-// 			</div>
-// 		</div>
-// 	</li>
-// );
+function TimelineList( props ) {
+	const timelines = props.timelines;
+	const items = timelines.map(( item, index ) => 
 
-const timeline = document.querySelector( '#timeline' );
+		<li className="growup-row" key={index}>
+			<div className="growup-left-col">
+				<p>
+					{item.date}
+				</p>
+			</div>
+			<div className="growup-right-col">
+				<div className="growup-right-col-wrapper">
+					<div className="timeline-line"></div>
+					<div className="timeline-wrapper">
+						<div className="timeline-circle-wrapper">
+							<div className="timeline-circle"></div>
+						</div>
+					</div>
+					<div className="growup-text-wrapper">
+						<h3 className="growup-title">
+							{item.title}
+						</h3>
+						<p>
+							{item.lead}
+						</p>
+					</div>
+				</div>
+			</div>
+		</li>
 
-// ReactDOM.render( timelineRow, timeline );
+	);
+	
+	return (
+		<ul className="growup-wrapper">{items}</ul>
+	);
+}
+
+const timeline = document.querySelector( '#timeline' ); 
+
+ReactDOM.render( <TimelineList timelines={timelineRows}/>, timeline ); 
+
