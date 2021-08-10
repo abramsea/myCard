4fa2893ba6b229977ad9353afafadd9c4b398933
Evgenii Abramson <abramson.ea@gmail.com>
2021-08-10 11:52:12 +0300

    Update 2021-08-10T08:52:10.733Z

--- js/app.js
+++ js/app.js
@@ -55,60 +55,33 @@ const cards = Object.values( projectsInfo ).map( item => item );
 
 function CardList( props ) {
 	const cards = props.cards;
-	const items = cards.map( function ( item, index ) {
-
-		if ( item.github != null ) 
-		{
-			return (
-				<li className="card" key={index++}>
-					<picture className="card__pic-box">
-						<img className="card__pic" src={item.image} alt={item.title}></img>
-					</picture>
-					<div className="card__darken">
-						<h3 className="card__title">{item.title}</h3>
-						<p className="card__tech">
-							{item.lead}
-						</p>
-						<p className="card__tech">
-							{item.tech}
-						</p>
-					</div>
-					<div className="card__info">
+	const items = cards.map(( item, index ) => 
+		(
+			<li className="card" key={index++}>
+				<picture className="card__pic-box">
+					<img className="card__pic" src={item.image} alt={item.title}></img>
+				</picture>
+				<div className="card__darken">
+					<h3 className="card__title">{item.title}</h3>
+					<p className="card__tech">
+						{item.lead}
+					</p>
+					<p className="card__tech">
+						{item.tech}
+					</p>
+				</div>
+				<div className="card__info">
+					{ item.github !== null ? 
 						<a href={item.github} className="icon" target="_blank">
 							<i className="fab fa-github"></i>
-						</a>
-						<a href={item.link} className="icon" target="_blank">
-							<i className="fas fa-external-link-alt"></i>
-						</a>
-					</div>
-				</li>
-			)
-		}
-		else
-		{
-			return (
-				<li className="card" key={index++}>
-					<picture className="card__pic-box">
-						<img className="card__pic" src={item.image} alt={item.title}></img>
-					</picture>
-					<div className="card__darken">
-						<h3 className="card__title">{item.title}</h3>
-						<p className="card__tech">
-							{item.lead}
-						</p>
-						<p className="card__tech">
-							{item.tech}
-						</p>
-					</div>
-					<div className="card__info">
-						<a href={item.link} className="icon" target="_blank">
-							<i className="fas fa-external-link-alt"></i>
-						</a>
-					</div>
-				</li>
-			)
-		}
-	});
+						</a> : null }
+					<a href={item.link} className="icon" target="_blank">
+						<i className="fas fa-external-link-alt"></i>
+					</a>
+				</div>
+			</li>
+		)
+	);
 
 	return (
 		<ul className="cards">{items}</ul>
